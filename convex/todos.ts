338e509c9c96fbab3f9650ed622fa3e95d99ec67
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-08-20 20:15:09 +0500

    Added Functions To Convex To Perform App Functionality

---
+++ convex/todos.ts
@@ -0,0 +1,71 @@
+import { query, mutation } from "./_generated/server";
+import { v, ConvexError } from "convex/values";
+
+export const getTodos = query({
+  handler: async (ctx) => {
+    const todos = await ctx.db.query("todos").order("desc").collect();
+    return todos;
+  },
+});
+
+export const addTodo = mutation({
+  args: {
+    text: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const todoId = await ctx.db.insert("todos", {
+      text: args.text,
+      isCompleted: false,
+    });
+    return todoId;
+  },
+});
+
+export const toggleTodo = mutation({
+  args: {
+    id: v.id("todos"),
+  },
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.id);
+    if (!todo) {
+      throw new ConvexError("Todo not found");
+    }
+
+    await ctx.db.patch(args.id, {
+      isCompleted: !todo.isCompleted,
+    });
+  },
+});
+
+export const deleteTodo = mutation({
+  args: {
+    id: v.id("todos"),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const updateTodo = mutation({
+  args: {
+    text: v.string(),
+    id: v.id("todos"),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.patch(args.id, {
+      text: args.text,
+    });
+  },
+});
+
+export const clearAllTodos = mutation({
+  handler: async (ctx) => {
+    const todos = await ctx.db.query("todos").collect();
+
+    todos.forEach(async (todo) => {
+      await ctx.db.delete(todo._id);
+    });
+
+    return { deleteCount: todos.length };
+  },
+});
