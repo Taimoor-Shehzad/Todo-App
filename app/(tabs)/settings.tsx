8dd7509f4c330f4545b0a1d89196f70627196eec
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-08-21 18:55:09 +0500

    Created Progress Stats Component

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,17 +1,41 @@
-import { Text } from "react-native";
-import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { createSettingsStyles } from "@/assets/styles/settings.styles";
 import useTheme from "@/hooks/useTheme";
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
+import ProgressStats from "@/components/ProgressStats";
 
 const Settings = () => {
-  const { colors } = useTheme();
+  const [isSync, setIsSync] = useState(true);
+  const [notifications, setNotification] = useState(true);
+
+  const { colors, toggleDarkMode } = useTheme();
   const styles = createSettingsStyles(colors);
 
   return (
-    <SafeAreaView>
-      <Text>settings</Text>
-    </SafeAreaView>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.header}>
+          <View style={styles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={styles.iconContainer}
+            >
+              <Ionicons name="settings" size={28} color="#fff" />
+            </LinearGradient>
+            <Text style={styles.title}>Settings</Text>
+          </View>
+        </View>
+        <ScrollView style={styles.scrollView}>
+          <ProgressStats />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
